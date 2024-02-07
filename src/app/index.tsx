203c5903533d91bd903a176dc6d64861675bc618
Julio Lima <lima.julio.cs@gmail.com>
2024-02-07 13:29:44 -0300

    structures menu, products, cart - class 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,42 @@
+import { View, FlatList, SectionList, Text } from "react-native";
+import { useState, useRef } from "react";
+import { Link } from "expo-router"
+
+import { useCartStore } from "@/stores/cart-store";
+
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
+import { Product } from "@/components/product";
 
-import { CATEGORIES } from "@/utils/data/products"
-import { useState } from "react";
+import { CATEGORIES, MENU } from "@/utils/data/products"
 
 export default function Home() {
-
+  const cartStore = useCartStore();
   const [category, setCategory] = useState('');
+
+  const sectionListRef = useRef<SectionList>(null);
+
+  const cartQuantityItems = cartStore.products.reduce(
+    (total, product) => total + product.quantity, 0
+  );
 
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory);
+
+    if(sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0
+      })
+    }
   }
 
   return (
     <View className="bg-slate-900 flex-1">
-      <Header title="Cardápio"cartQuantityItems={5}/>
+      <Header title="Cardápio" cartQuantityItems={cartQuantityItems}/>
 
       <FlatList 
         data={CATEGORIES} 
@@ -25,6 +46,26 @@ export default function Home() {
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator = {false}
+      />
+
+      <SectionList 
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            { title }
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
---
+++ src/app/product/[id].tsx
@@ -0,0 +1,64 @@
+import { Image, Text, View } from "react-native";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useCartStore } from "@/stores/cart-store";
+import { Feather } from "@expo/vector-icons"
+
+import { PRODUCTS } from "@/utils/data/products"; 
+import { FormatCurrency } from "@/utils/functions/formatCurrency";
+
+import { Button } from "@/components/button";
+import { LinkButton } from "@/components/link-button";
+
+export default function Product() {
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
+  const { id } = useLocalSearchParams();
+
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
+
+  function handleAddCart() {
+    cartStore.add(product);
+    navigation.goBack();
+  }
+
+  return(
+    <View className="flex-1">
+      <Image 
+        source={product.cover} 
+        className="w-full h-52" 
+        resizeMode="cover"
+      />
+
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2xl font-heading my-1">
+          { FormatCurrency(product.price) }
+        </Text>
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+          { product.description }
+        </Text>
+
+        {  product.ingredients.map((ingredient) => (
+          <Text 
+            key={ingredient} 
+            className="text-slate-400 font-body text-base leading-6"
+          >
+            {"\u2022"} {ingredient}
+          </Text>
+        )) }
+      </View>
+
+      <View className="p-5 pb-8 gap-5">
+        <Button onPress={handleAddCart}>
+          <Button.Icon>
+            <Feather name="plus-circle" size={20} />
+          </Button.Icon>
+          <Button.Text>
+            Adicionar ao pedido
+          </Button.Text>
+        </Button>
+
+        <LinkButton title="voltar ao cardápio" href="/" />
+      </View>
+    </View>
+  )
+}
